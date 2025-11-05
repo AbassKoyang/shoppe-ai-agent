214c7a68479f1addc6112bb0aa1873de8a2c2283
Abass Koyang Ahmad <abasskoyang12345@gmail.com>
2025-11-05 02:03:24 +0100

    add firebase file

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,11 +1,15 @@
-import admin from 'firebase-admin';
+import { initializeApp, getApps} from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-import serviceAccount from './ServiceAccountKey.json' with { type: 'json' };
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+};
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-});
-
-export const db = admin.firestore();
-export const messaging = admin.messaging();
-export const FieldValue = admin.firestore.FieldValue;
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+export const db = getFirestore(app);
