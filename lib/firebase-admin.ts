63a69f61d068dc457077e6662b10782b91b33ed3
Abass Koyang Ahmad <abasskoyang12345@gmail.com>
2025-11-05 09:22:39 +0100

    fix service account key import issues

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,6 +1,6 @@
 import admin from 'firebase-admin';
 
-const serviceAccount = require('./ServiceAccountKey.json') as admin.ServiceAccount;
+import serviceAccount from './ServiceAccountKey.json' with { type: 'json' };
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
